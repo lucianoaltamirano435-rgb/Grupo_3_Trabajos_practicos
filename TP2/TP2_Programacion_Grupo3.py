ec62dc4d2066af2d52ab98d86b217933bc8782dd
lucianoaltamirano435-rgb <lucianoaltamirano435@gmail.com>
2026-07-16 10:28:03 -0500

    Creación de los archivos de trabajo
    Se crearon los archivos del informe final y un Script sectorizado de Python para el desarrollo del código.

---
+++ TP2/TP2_Programacion_Grupo3.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Universidad de Buenos Aires - Taller de Programación
+Trabajo Práctico N° 2: Métodos No Supervisados usando la EPH
+
+Grupo 3:
+    - Luciano Altamirano (Parte I y Parte II.A)
+    - Gonzalo Pasiche   (Parte II.B)
+
+Fecha de entrega: viernes 24 de julio de 2026 a las 13:00 h.
+"""
+
+# =============================================================================
+# PAQUETES
+# =============================================================================
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+# =============================================================================
+# PARTE I: CREACIÓN DE VARIABLES Y MÁS ESTADÍSTICA DESCRIPTIVA
+# (Luciano Altamirano)
+# =============================================================================
+
+
+
+
+# =============================================================================
+# PARTE II: MÉTODOS NO SUPERVISADOS
+# =============================================================================
+
+# =============================================================================
+# PARTE II.A: ANÁLISIS DE COMPONENTES PRINCIPALES (PCA)
+# (Luciano Altamirano)
+# =============================================================================
+
+
+
+
+# =============================================================================
+# PARTE II.B: ANÁLISIS CLÚSTER
+# (Gonzalo Pasiche)
+# =============================================================================
+
+
+
+
